d7b7cac549f902c50d1d1b8dcd761005ea7d4320
Torrey Brett <119777006+6tee@users.noreply.github.com>
2022-12-04 08:52:51 -0600

    first API framework
    creation of framework for first API

---
+++ backend/routes/fightData.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+//importing data model schemas
+let { eventdata } = require("../models/models"); 
+
+//GET all entries
+router.get("/", (req, res, next) => { 
+    eventdata.find( {organization_id: process.env.ORGANIZATION}, //Filters events to show events created within current organization instance
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
+});
+
+module.exports = router;
